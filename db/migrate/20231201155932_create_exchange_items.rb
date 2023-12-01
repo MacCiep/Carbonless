c8f138ae47a6329204fdab74fca8f8557d14ec2e
Mioszek <228871@edu.p.lodz.pl>
2023-12-01 17:18:46 +0100

    addmigration and model - change schema to include newest migration

---
+++ db/migrate/20231201155932_create_exchange_items.rb
@@ -0,0 +1,11 @@
+class CreateExchangeItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :exchange_items do |t|
+      t.string :name, null: false, limit: 80
+      t.string :description, limit: 300
+      t.belongs_to :user, null: false, foreign_key: true
+      t.integer :status, default: 0
+      t.timestamps
+    end
+  end
+end
